50523bca4ceca695f0ee90096600101a3d1f0305
w15677310528 <38684508+w15677310528@users.noreply.github.com>
2018-05-02 18:59:24 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "欢迎使用一键搭建"
-echo "即将搭建的是4.26-5.10版本"
+echo "即将搭建的是5.10版本"
 echo "准备开始安装"
 read -p "回车后开始安装"
 echo "请输入你的内网ip" 
@@ -36,5 +36,3 @@ npm install -g forever
 forever start index.js sniff eth0 $ip | pino
 
 echo "搭建完成"
-echo "并不是一个人在战斗，加入我们6698530@qq.com"
-echo "你能加上你的QQ，但加不上我的心"
